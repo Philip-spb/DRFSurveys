12e109490b1d6fbee201ebe94868988b2beeed57
Philip-spb <philip.spb@gmail.com>
2020-10-21 11:25:03 +0300

    Добавлен тест проверяющий возможность создания при помощи API нового опроса

--- api/tests.py
+++ api/tests.py
@@ -1,3 +1,40 @@
-from django.test import TestCase
+from rest_framework.authtoken.admin import User
+from rest_framework.test import APITestCase
+from rest_framework import status
+from rest_framework.reverse import reverse
 
-# Create your tests here.
+from .models import *
+
+
+class SurveyTestCase(APITestCase):
+
+    def setUp(self):
+        # Создаем нового пользвователя в нашем приложении
+        user = User.objects.create(username='new_user', email='new_user@gmail.com')
+        user.set_password('HelloWorld')
+        user.save()
+
+    def test_create_survey_api(self):
+        # Логинимся нашим пользователем в системе
+        url = reverse('api:login')
+        data = {
+            'username': 'new_user',
+            'password': 'HelloWorld',
+        }
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        # Получаем токен для доступа к функциям администратора (например для создания новых опросов)
+        token = response.data.get('token', 0)
+        token_len = len(token)
+        self.assertGreater(token_len, 0)
+
+        # Создаем новый "Тестовый опрос" при помощи API
+        url = reverse('api:surveys-list')
+        data = {'name': 'Тестовй опрос',}
+        response = self.client.post(url, data, format='json',  HTTP_AUTHORIZATION='Token {}'.format(token))
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+        # Проверяем что новый "Тестовый опрос" создался
+        qs = Survey.objects.filter(name='Тестовй опрос')
+        self.assertEqual(qs.count(), 1)
--- api/urls.py
+++ api/urls.py
@@ -4,8 +4,10 @@ from rest_framework.authtoken import views
 
 from .views import *
 
+app_name = 'api'
+
 router = DefaultRouter()
-router.register(r'surveys', SurveyViewSet)
+router.register(r'surveys', SurveyViewSet, basename='surveys')
 router.register(r'active-surveys', ActiveSurveyViewSet)
 router.register(r'full-surveys', FullSurveyViewSet)
 router.register(r'questions', QuestionViewSet)
@@ -16,7 +18,7 @@ urlpatterns = [
     path('test-base/', TestBaseView.as_view()),
     path('test-base/<int:pk>/', SingleTestBaseView.as_view()),
     path('single-user-test-base/<int:user>/', SingleUserTestBaseView.as_view()),
-    path('login/', views.obtain_auth_token),
+    path('login/', views.obtain_auth_token, name='login'),
 ]
 
 urlpatterns += router.urls
